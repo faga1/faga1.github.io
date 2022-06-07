98651131f77e2d192ccc7b0dd5fe419797c92255
faga1 <1064827638@qq.com>
2022-06-07 16:49:53 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    title: "faga's blog",
+    description: 'Just playing around',
+    themeConfig: {
+      sidebarDepth:2,
+      searchMaxSuggestions: 10,
+      nav: [
+        { text:'主页', link:'/' },
+        { text: '笔记', link: '/blog/' },
+        { text: 'Github', link: 'https://github.com/faga1' }
+      ],
+      sidebar: {
+        '/blog/':[
+          '',
+          'react',
+          'web_upload_oss',
+          'webpack',
+          'react',
+          'interview',
+          'commonjs&esmodule'
+        ]
+      }
+    }
+}
